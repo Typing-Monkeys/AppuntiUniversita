92783303781c8570c48657afa419ece72f889adf
Nicolò Vescera <nicolo.vescera@gmail.com>
2021-11-03 14:21:05 +0100

    Create autoinstall.py
    Script per autosetup delle macchine

---
+++ Quarto Anno/HPC/2_fedoraman/autoinstall.py
@@ -0,0 +1,58 @@
+from os import system
+
+def main():
+    # creazione nuovo utente
+    system('useradd osvaldo')
+    system('passwd osvaldo')
+    system('usermod -aG wheel osvaldo')
+
+    # aggiorno il sistema
+    system('dnf -y upgrade')
+    
+    # installo pacchetti
+    system('dnf -y install pacemaker corosync pcs')
+    system('dnf -y install drbd-pacemaker drbd-udev')
+    system('dnf -y install httpd')
+    system('dnf -y install iptables-services')
+
+    # ip statico
+    system('ifconfig')
+    ip = input('Inserisci ip della macchina: ')
+    scheda = input('Inserisci la scheda di rete: ')
+    gateway = input('Inserisci il gateway: ')
+
+    system(f"nmcli connection modify {scheda} IPv4.address {ip}/24")
+    system(f"nmcli connection modify {scheda} IPv4.gateway {gateway}")
+    system(f"nmcli connection modify {scheda} IPv4.dns 8.8.8.8")
+    system(f"nmcli connection modify {scheda} IPv4.method manual")
+    
+    # modifica di hosts
+    macchina_principale = input("Sei nella macchina principale ? [s/n]: ")
+    ip2 = input("Inserisci ip della macchina 2: ")
+
+    if macchina_principale == "s":
+        system(f"echo \"{ip}\tFedoraman\" >> /etc/hosts")
+        system(f"echo \"{ip2}\tFedoragirl\" >> /etc/hosts")
+    else:
+        system(f"echo \"{ip}\tFedoragirl\" >> /etc/hosts")
+        system(f"echo \"{ip2}\tFedoraman\" >> /etc/hosts")
+
+    # disabilitare firewall
+    system('systemctl disable firewalld')
+
+    # pcsd config
+    system('passwd hacluster')
+    system('systemctl enable pcsd')
+    system('pcs client local-auth')
+    system('pcs cluster auth')
+
+    input("Continuare quando sei arrivato a questo punto anche sulla seconda macchina")
+
+    system('pcs cluster setup ExampleCluster node1 node2')
+    
+    if macchina_principale == "s":
+        system('pcs start --all')
+        system('pcs cluster enable --all')
+    
+if __name__ == "__main__":
+    main()
